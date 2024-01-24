ae7e3753552e88032d5bd0c29b5754b044818e47
Jaysahhn <jasonmason96@yahoo.com>
2024-01-24 00:26:57 -0600

    added files for portfolio. finished portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,27 +1,14 @@
 export default function PortfolioPage() {
-
-    // Iterate over each mock user to display their abridged profile data and a link to their page
     return (
         <div className="container  d-flex justify-content-center">
-
-
             <div class="row justify-content-center m-3">
                 <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
                 <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-12-Jasons-EmployeeTracker">EmployeeTracker</a> <a href="https://drive.google.com/file/d/1ZWDoxSEjugtozXQaWJTVH4a9-L5-Rqy8/view"><img src="public/assets/EmployeeTrackerProject - Copy.png" className="img-fluid" to='https://drive.google.com/file/d/1ZWDoxSEjugtozXQaWJTVH4a9-L5-Rqy8/view'></img></a></div>
                 <div class="col-sm-3 m-3"><a href="https://github.com/drewhouses/freddys-flick-picks">Freddy's Flick Picks</a> <a href="https://drewhouses.github.io/freddys-flick-picks/"><img src="public/assets/FreddyFlickPickProject - Copy.jpg" className="img-fluid" to='https://drewhouses.github.io/freddys-flick-picks/'></img></a></div>
-                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
-                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
-                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
-
-                {/* <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
-                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
-                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
-                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
-                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div> */}
-
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-11-Jasons-NoteTaker">NoteTaker</a> <a href="https://intense-ocean-41146-c8ded9b4cdde.herokuapp.com/"><img src="public/assets/NoteTaker - Copy.png" className="img-fluid" to='https://intense-ocean-41146-c8ded9b4cdde.herokuapp.com/'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-5-Jasons-Interactive-Scheduler">Interactive Scheduler</a> <a href="https://jaysahhn.github.io/Week-5-Jasons-Interactive-Scheduler/"><img src="public/assets/InteractiveScheduler - Copy.png" className="img-fluid" to='https://jaysahhn.github.io/Week-5-Jasons-Interactive-Scheduler/'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-18-Jasons-SocialNetworkAPI">Social Network API</a> <a href="https://drive.google.com/file/d/1kyWQx1-qhMB_ogSlhylZfyzku5-vKsdS/view"><img src="public\assets\SocialNetworkAPI - Copy.jpg" className="img-fluid" to='https://drive.google.com/file/d/1kyWQx1-qhMB_ogSlhylZfyzku5-vKsdS/view'></img></a></div>
             </div>
-
-
         </div>
     );
 }
