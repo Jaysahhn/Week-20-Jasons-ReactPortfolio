632beac9c135b5202fb68dc5b6d71b905bc3605b
Jaysahhn <jasonmason96@yahoo.com>
2024-01-24 00:48:22 -0600

    added filler resume to resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,9 +1,9 @@
-export default function ResumePage() {
+export default function HomePage() {
 
     // Iterate over each mock user to display their abridged profile data and a link to their page
     return (
-        <div className="container pt-4 text-center">
-            <h1>resume page</h1>
+        <div className="container pt-4">
+            <div class="col-sm-3 m-3"><a href="public/assets/RESUMEFILLER.pdf">EXAMPLE RESUME</a> <a href="public/assets/RESUMEFILLER.pdf"><img src="public/assets/resumeword.jpg" className="img-fluid" to='public/assets/RESUMEFILLER.pdf'></img></a></div>
         </div>
     );
 }
