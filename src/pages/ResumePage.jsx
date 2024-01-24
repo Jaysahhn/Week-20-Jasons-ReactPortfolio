a663d71612c2940a9ff25f47083997736b1a8e75
Jaysahhn <jasonmason96@yahoo.com>
2024-01-23 22:37:33 -0600

    modified pages

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,9 @@
+export default function ResumePage() {
+
+    // Iterate over each mock user to display their abridged profile data and a link to their page
+    return (
+        <div className="container pt-4">
+
+        </div>
+    );
+}
