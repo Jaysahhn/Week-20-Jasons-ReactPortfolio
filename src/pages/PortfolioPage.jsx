c189360bae24cdaaf5af5971306964799f18c0c4
Jaysahhn <jasonmason96@yahoo.com>
2024-01-24 00:14:05 -0600

    partially finished portfolio page

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -2,8 +2,26 @@ export default function PortfolioPage() {
 
     // Iterate over each mock user to display their abridged profile data and a link to their page
     return (
-        <div className="container pt-4 text-center">
-            <h1>portfolio page</h1>
+        <div className="container  d-flex justify-content-center">
+
+
+            <div class="row justify-content-center m-3">
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-12-Jasons-EmployeeTracker">EmployeeTracker</a> <a href="https://drive.google.com/file/d/1ZWDoxSEjugtozXQaWJTVH4a9-L5-Rqy8/view"><img src="public/assets/EmployeeTrackerProject - Copy.png" className="img-fluid" to='https://drive.google.com/file/d/1ZWDoxSEjugtozXQaWJTVH4a9-L5-Rqy8/view'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/drewhouses/freddys-flick-picks">Freddy's Flick Picks</a> <a href="https://drewhouses.github.io/freddys-flick-picks/"><img src="public/assets/FreddyFlickPickProject - Copy.jpg" className="img-fluid" to='https://drewhouses.github.io/freddys-flick-picks/'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
+                <div class="col-sm-3 m-3"><a href="https://github.com/Jaysahhn/Week-19-Jasons-PWATextEditor">J.A.T.E</a> <a href="https://asimplepwatexteditor.onrender.com/"><img src="public/assets/JATEProject - Copy.png" className="img-fluid" to='https://asimplepwatexteditor.onrender.com/'></img></a></div>
+
+                {/* <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
+                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
+                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
+                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div>
+                <div class="col-sm-3 m-3"><img src="/assets/Bijbanner2 .png" className="img-fluid"></img></div> */}
+
+            </div>
+
+
         </div>
     );
 }
