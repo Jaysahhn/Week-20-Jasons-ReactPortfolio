3f15f049b8814549f62b066dd891ece2c1df9b1f
Jaysahhn <jasonmason96@yahoo.com>
2024-01-24 01:00:22 -0600

    filled out contactpage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,25 @@
 export default function ContactPage() {
 
-  // Iterate over each mock user to display their abridged profile data and a link to their page
   return (
     <div className="container pt-4 text-center">
-      <h1>contact page</h1>
+      <h1>Contact Me!</h1>
+      <div className="contact-info">
+        <div>
+          <p>Want to get into contact?</p>
+          <address>
+            San Antonio, TX <br />
+            P: <a href="tel:210.418.9836">210.418.9836</a>
+            <br />
+            E:{" "}
+            <a href="mailto://jasonmason96@yahoo.com">
+              jasonmason96@yahoo.com
+            </a>
+          </address>
+          <p>
+            <strong>I'd love to hear your feedback!</strong>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
