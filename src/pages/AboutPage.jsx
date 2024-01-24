f4a866f3f5cfbfa8c0bea794e3916a08c0c99085
Jaysahhn <jasonmason96@yahoo.com>
2024-01-23 23:22:02 -0600

    finished aboutpage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,8 +2,30 @@ export default function AboutPage() {
 
   // Iterate over each mock user to display their abridged profile data and a link to their page
   return (
-    <div className="container pt-4 text-center">
-      <h1>Jason Mason bio</h1>
+    <div class="flex-row">
+      <h1 id="about" className="section-title primary-border">
+        About Me
+      </h1>
+      <p>
+        Greetings! I'm Jason Mason, a dynamic 27-year-old professional currently immersed in the ever-evolving realm of the medical field. While my journey has been rooted in laboratories and healthcare, I find myself at the exciting crossroads of change, driven by a fervent desire to venture into the world of software development.
+      </p>
+      <p>
+        I call the vibrant city of San Antonio, Texas, my home, where the rich blend of culture and innovation mirrors my own aspirations. Beyond the world of coding, I am passionate about dachshunds and am the proud owner of a charming one named Bennie.
+      </p>
+      <p>
+        In my current role, I've honed my analytical and detail-oriented skills, ensuring precision in laboratory processes. However, my insatiable curiosity and passion for technology have sparked a new aspiration – to transition into the dynamic field of software development.
+      </p>
+      <p>
+        I envision a future where my analytical prowess and dedication translate seamlessly into crafting innovative software solutions. Eager to embark on this transformative journey, I am actively acquiring new skills and staying abreast of the latest trends in programming languages, web development, and software engineering.
+      </p>
+      <p>
+        Outside the realm of coding, you'll find me navigating virtual landscapes in the gaming world, where my strategic thinking and problem-solving skills are put to the test. Balancing work and play, I am equally committed to maintaining a healthy lifestyle through regular workouts, fostering resilience and focus in all aspects of life.
+      </p>
+      <p>
+        Join me on this exciting adventure as I embrace the challenges and opportunities that software development has to offer, all while cherishing the companionship of Bennie, my delightful dachshund, in the heart of San Antonio, Texas.
+      </p>
+
+
     </div>
   );
 }
